ee4af6579ce729e3d172713b07b2664ca6fc13f4
Blas Piris <bpirisr@gmail.com>
2021-02-07 16:34:07 +0100

    Guardado mañana 07-02-21

--- js/script_2.js
+++ js/script_2.js
@@ -1,6 +1,6 @@
 //API F1
 
-let container2=document.querySelector(".content__2")
+let container=document.querySelector(".content")
 
 const apiF1Equipos=()=>{
     fetch("https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=Formula%201")
@@ -12,8 +12,8 @@ const apiF1Equipos=()=>{
 }
 
 const mostrarEquipos=(equipos)=>{
-    container2.innerHTML="";
-    container2.className="content__2"
+    container.innerHTML="";
+    container.className="content"
     for(equipo of equipos){
        
 
@@ -29,7 +29,7 @@ const mostrarEquipos=(equipos)=>{
         title.textContent=equipo.strTeam;
         article.appendChild(title);
 
-    container2.appendChild(article);
+    container.appendChild(article);
     }
 }
 
@@ -45,8 +45,8 @@ const apiF1Search=()=>{
 
 
 const apiF1Pilotos=(drivers)=>{
-    container2.className="content__2";
-    container2.innerHTML="";
+    container.className="content";
+    container.innerHTML="";
     for(piloto of drivers){
         fetch("https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p="+piloto.givenName+" "+piloto.familyName)
         .then(response => response.json())
@@ -71,69 +71,9 @@ const mostrarPilotos=(piloto)=>{
          title.textContent=piloto.strPlayer;
          article.appendChild(title);
 
-     container2.appendChild(article);
+     container.appendChild(article);
     
 }
-
-const apiF1Temporada=()=>{
-    fetch("https://www.thesportsdb.com/api/v1/json/1/eventsseason.php?id=4370&s=2021")
-    .then(response => response.json())
-    .then(responseJSON => {
-    let data = responseJSON;
-    console.log(data.events)  
-    mostrarTemporada(data.events)    
-    });  
-}
-
-const mostrarTemporada=(temporada)=>{
-    container2.innerHTML="";
-    container2.className="content__2__calendar";
-        
-        let title4=document.createElement("a");
-        title4.className="calendar__text__title"
-        title4.textContent="Nº";
-        container2.appendChild(title4);
-
-        let title=document.createElement("a");
-        title.className="calendar__text__title"
-        title.textContent="GRAN PREMIO";
-        container2.appendChild(title);
-
-        let title2=document.createElement("a");
-        title2.className="calendar__text__title"
-        title2.textContent="FECHA"
-        container2.appendChild(title2);
-        
-        let title3=document.createElement("a");
-        title3.className="calendar__text__title"
-        title3.textContent="CIRCUITO";
-        container2.appendChild(title3);
-    
-    for(gp of temporada){
-        let title4=document.createElement("a");
-        title4.className="calendar__text"
-        title4.textContent=gp.intRound;
-        container2.appendChild(title4);
-
-        let title=document.createElement("a");
-        title.className="calendar__text"
-        title.textContent=gp.strEvent;
-        container2.appendChild(title);
-
-        let title2=document.createElement("a");
-        title2.className="calendar__text"
-        title2.textContent=gp.dateEvent;
-        container2.appendChild(title2);
-        
-        let title3=document.createElement("a");
-        title3.className="calendar__text"
-        title3.textContent=gp.strVenue;
-        container2.appendChild(title3);
-
-    }
-}
-
-
 
 document.addEventListener("DOMContentLoaded",()=>{
     apiF1Equipos();
@@ -143,9 +83,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
         if(event.target.id=="drivers"){
             apiF1Search();
-        }
-        if(event.target.id=="calendar"){
-            apiF1Temporada();
         }
     })
 });
