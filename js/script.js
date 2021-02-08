820dcb8afb8c6f3028080d38b7d98638db282228
Blas Piris <bpirisr@gmail.com>
2021-02-08 01:50:07 +0100

    Practica Preparada

--- js/script.js
+++ js/script.js
@@ -43,9 +43,11 @@ const apiSearchLibrary=(search)=>{
 const mostrarInfo=(data)=>{
    
     container.innerHTML="";
+    container.className="data__container__books"
 
    
     for(book of data) {
+
         let article=document.createElement("article");
         article.className="article__book";
 
@@ -56,19 +58,72 @@ const mostrarInfo=(data)=>{
             img.setAttribute("src","../imagenes/sinportada.jpg")
         }
         img.classList.add("fotos_res");
+        img.id=book.id
         article.appendChild(img);
 
         let title=document.createElement("h5");
         title.textContent=book.volumeInfo.title;
         title.classList.add("title__book")
+        title.id=book.id
         article.appendChild(title);
 
         
     container.appendChild(article);
     }
+}
 
+const searchBook=(id)=>{
+    fetch("https://www.googleapis.com/books/v1/volumes/"+id)
+    .then(response => response.json())
+    .then(responseJSON => {
+	let data = responseJSON;
+    console.log(data)
+    mostrarBook(data)
+    }); 
+}
 
-   
+const mostrarBook=(book)=>{
+    container.innerHTML="";
+    container.className="book__info"
+
+    let article=document.createElement("article");
+        article.className="article__book__data";
+
+        let img=document.createElement("img");
+        if(book.volumeInfo.imageLinks!=undefined)
+            img.setAttribute("src",book.volumeInfo.imageLinks.thumbnail)
+        else{
+            img.setAttribute("src","../imagenes/sinportada.jpg")
+        }
+        img.classList.add("fotos_res");
+        img.id=book.id
+        article.appendChild(img);
+
+        let divInfo=document.createElement("div")
+
+            let title=document.createElement("h5");
+            title.textContent="Titulo: "+book.volumeInfo.title;
+            title.classList.add("book__info__text")
+            divInfo.appendChild(title);
+
+            let title2=document.createElement("h5");
+            title2.textContent="Autor: "+book.volumeInfo.authors;
+            title2.classList.add("book__info__text")
+            divInfo.appendChild(title2);
+
+            let title3=document.createElement("h5");
+            title3.textContent="Editorial: "+book.volumeInfo.publisher;
+            title3.classList.add("book__info__text")
+            divInfo.appendChild(title3);
+
+            let title4=document.createElement("h5");
+            title4.innerHTML="Resumen: "+book.volumeInfo.description;
+            title4.classList.add("book__info__text")
+            divInfo.appendChild(title4);
+
+        article.appendChild(divInfo)
+    container.appendChild(article);
+
 }
 
 
@@ -76,6 +131,12 @@ const mostrarInfo=(data)=>{
 document.addEventListener("DOMContentLoaded",()=>{
     apiSearchLibrary(arrayBusqueda[Math.floor(Math.random()*arrayBusqueda.length)]);
     formInfo();
+})
+
+document.addEventListener("click",(event)=>{
+    if(event.target.className=="title__book" || event.target.className=="fotos_res"){
+        searchBook(event.target.id)
+    }
 })
 
    
